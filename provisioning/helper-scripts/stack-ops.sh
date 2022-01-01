738f821826b60f0bab8d7225ae9229e5406db3b5
TomoyukiSugiyama <s_tomoyuki07@yahoo.co.jp>
2022-01-01 23:50:21 +0900

    add validate option

--- provisioning/helper-scripts/stack-ops.sh
+++ provisioning/helper-scripts/stack-ops.sh
@@ -2,9 +2,9 @@
 # ref: https://www.slideshare.net/yktko/cloudformation-getting-started-with-yaml
 
 mode=$1; shift
-stack_name=$1; shift
-template=$1; shift
-if [ "$mode" != "create" -a "$mode" != "update" -a "$mode" != "delete" -a "$mode" != "list" -a "$mode" != "describe" ]; then
+arg1=$1; shift
+arg2=$1; shift
+if [ "$mode" != "create" -a "$mode" != "update" -a "$mode" != "delete" -a "$mode" != "list" -a "$mode" != "describe" -a "$mode" != "validate" ]; then
     echo ""
     echo "Usage: $0 MODE ARGS"
     echo ""
@@ -13,19 +13,14 @@ if [ "$mode" != "create" -a "$mode" != "update" -a "$mode" != "delete" -a "$mode
     echo "update    stack-name path-to-cfn-template-file [param1=val1 param2=val2]"
     echo "list      ";
     echo "describe  stack-name";
+    echo "validate  path-to-cfn-template-file"
     echo "delete    stack-name"; exit 1
 fi
 
 if [ "$mode" == "create" -o "$mode" == "update" ]; then
     params=$(echo $* | perl -pe "s/([^= ]+)=([^ ]+)/ParameterKey=\1,ParameterValue=\2/g")
-    args="--template-body file://${template} --capabilities CAPABILITY_IAM ${params}"
-    stack_name_option="--stack-name ${stack_name}"
-    mode_option="${mode}-stack"
-fi
-
-if [ "$mode" = "delete" ]; then
-    args=""
-    stack_name_option="--stack-name ${stack_name}"
+    args="--template-body file://${arg2} --capabilities CAPABILITY_IAM ${params}"
+    stack_name_option="--stack-name ${arg1}"
     mode_option="${mode}-stack"
 fi
 
@@ -37,10 +32,21 @@ fi
 
 if [ "$mode" == "describe" ]; then
     args=""
-    stack_name_option="--stack-name ${stack_name}"
+    stack_name_option="--stack-name ${arg1}"
     mode_option="${mode}-stacks"
 fi
 
+if [ "$mode" = "validate" ]; then
+    args="--template-body file://${arg1}"
+    stack_name_option=""
+    mode_option="${mode}-template"
+fi
+
+if [ "$mode" = "delete" ]; then
+    args=""
+    stack_name_option="--stack-name ${arg1}"
+    mode_option="${mode}-stack"
+fi
 cmd="aws cloudformation ${mode_option} ${stack_name_option} ${args}"
 echo ${cmd}
 eval ${cmd}
