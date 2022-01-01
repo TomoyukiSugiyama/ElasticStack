3e1cb7a125d9c732fce52811ef9c58f7fee4f5e7
TomoyukiSugiyama <s_tomoyuki07@yahoo.co.jp>
2022-01-01 23:35:55 +0900

    fix var

--- provisioning/helper-scripts/stack-ops.sh
+++ provisioning/helper-scripts/stack-ops.sh
@@ -6,41 +6,41 @@ stack_name=$1; shift
 template=$1; shift
 if [ "$mode" != "create" -a "$mode" != "update" -a "$mode" != "delete" -a "$mode" != "list" -a "$mode" != "describe" ]; then
     echo ""
-    echo "Usage: $0 MODE [OPTIONS]"
+    echo "Usage: $0 MODE ARGS"
     echo ""
-    echo "Mode      [OPTIONS]"
-    echo "create    stack-name cfn-template-name [param1=val1 param2=val2]"
-    echo "update    stack-name cfn-template-name [param1=val1 param2=val2]"
-    echo "list      stack-name";
+    echo "Mode:     Args:"
+    echo "create    stack-name path-to-cfn-template-file [param1=val1 param2=val2]"
+    echo "update    stack-name path-to-cfn-template-file [param1=val1 param2=val2]"
+    echo "list      ";
     echo "describe  stack-name";
     echo "delete    stack-name"; exit 1
 fi
 
 if [ "$mode" == "create" -o "$mode" == "update" ]; then
     params=$(echo $* | perl -pe "s/([^= ]+)=([^ ]+)/ParameterKey=\1,ParameterValue=\2/g")
-    options="--template-body file://${template} --capabilities CAPABILITY_IAM ${params}"
+    args="--template-body file://${template} --capabilities CAPABILITY_IAM ${params}"
     stack_name_option="--stack-name ${stack_name}"
     mode_option="${mode}-stack"
 fi
 
 if [ "$mode" = "delete" ]; then
-    options=""
+    args=""
     stack_name_option="--stack-name ${stack_name}"
     mode_option="${mode}-stack"
 fi
 
 if [ "$mode" == "list" ]; then
-    options="--stack-status-filter CREATE_COMPLETE"
+    args="--stack-status-filter CREATE_COMPLETE"
     stack_name_option=""
     mode_option="${mode}-stacks"
 fi
 
 if [ "$mode" == "describe" ]; then
-    options=""
+    args=""
     stack_name_option="--stack-name ${stack_name}"
     mode_option="${mode}-stacks"
 fi
 
-cmd="aws cloudformation ${mode_option} ${stack_name_option} ${options}"
+cmd="aws cloudformation ${mode_option} ${stack_name_option} ${args}"
 echo ${cmd}
 eval ${cmd}
