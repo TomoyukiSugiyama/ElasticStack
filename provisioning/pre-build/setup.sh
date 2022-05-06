e743bb1f1abf65782a6428cb6cba9e778be18e36
TomoyukiSugiyama <s_tomoyuki07@yahoo.co.jp>
2022-05-06 15:41:28 +0900

    add s3 bucket

--- provisioning/pre-build/setup.sh
+++ provisioning/pre-build/setup.sh
@@ -9,22 +9,18 @@ alias cfn-stack-ops="$WORK_DIR/provisioning/helper-scripts/cfn-stack-ops.sh $1"
 # Create kms to encrypt/decrypt logs.
 # cfn-stack-ops deploy cmk kms.yaml
 
-# ECR
-# Create elastic container registory.
-# cfn-stack-ops deploy ecr $SCRIPT_DIR/ecr.yaml
-
 # SSM
 # Create parameters.
-# Need to set github connection id and slack workspace/channel ids in secrets/*.yaml , before create parameters.
-# $WORK_DIR/secrets/secret.sh
+# Need to set github connection id and slack workspace/channel ids in secrets/*.yaml, before create parameters.
+$WORK_DIR/secrets/secret.sh
 
 # S3
 # Get s3 bucket name from ssm.
-S3CfnBacketName=$(aws ssm get-parameter --name /dev/s3/cfn/BacketName --query "Parameter.Value" --output text)
-S3LambdaBacketName=$(aws ssm get-parameter --name /dev/s3/lambda/BacketName --query "Parameter.Value" --output text)
+S3CfnBucketName=$(aws ssm get-parameter --name /dev/s3/cfn/BucketName --query "Parameter.Value" --output text)
+S3LambdaBucketName=$(aws ssm get-parameter --name /dev/s3/lambda/BucketName --query "Parameter.Value" --output text)
 # Create s3 backets
+cfn-stack-ops deploy s3 $SCRIPT_DIR/cfn/s3.yaml S3CfnBucketName=$S3CfnBucketName S3LambdaBucketName=$S3LambdaBucketName
 
-
-
-
-
+# ECR
+# Create elastic container registory.
+cfn-stack-ops deploy ecr $SCRIPT_DIR/cfn/ecr.yaml
