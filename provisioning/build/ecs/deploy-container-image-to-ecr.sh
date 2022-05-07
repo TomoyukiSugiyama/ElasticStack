891ca497786272284eab4489345e758916bb2280
TomoyukiSugiyama <s_tomoyuki07@yahoo.co.jp>
2022-05-07 14:25:20 +0900

    fix

--- provisioning/build/ecs/deploy-container-image-to-ecr.sh
+++ provisioning/build/ecs/deploy-container-image-to-ecr.sh
@@ -4,7 +4,7 @@ docker context use default
 
 aws ecr get-login-password --region ${AWS_REGION}| docker login --username AWS --password-stdin ${ECR_URI}
 
-for {SERVICE} in logstash ecs-searchdomain-sidecar;
+for SERVICE in logstash ecs-searchdomain-sidecar;
 do
   docker image build -t ${ECR_URI}:${SERVICE} ${SERVICE}/
   docker image push ${ECR_URI}:${SERVICE}
--- provisioning/build/lambda/build.sh
+++ provisioning/build/lambda/build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-for {SERVICE} in populate-alb-tg-with-opensearch detach-task-to-be-terminated-from-nlb;
+for SERVICE in populate-alb-tg-with-opensearch detach-task-to-be-terminated-from-nlb;
 do
   (cd ${SERVICE} ; GOOS=linux go build ${SERVICE}.go)
   (cd ${SERVICE} ; zip ${SERVICE}.zip ${SERVICE})
