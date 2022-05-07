a11daa8a7ab02703fc185e95acbc177482aacdc5
TomoyukiSugiyama <s_tomoyuki07@yahoo.co.jp>
2022-05-07 14:16:40 +0900

    fix path

--- cfn-guard-rules/run-cfn-guard.sh
+++ cfn-guard-rules/run-cfn-guard.sh
@@ -2,5 +2,5 @@
 
 for file in `\find . -name '*.guard'`; do
     filename=`basename $file .guard`
-    cat ../provisioning/cfn/$filename.yaml | cfn-guard validate -r $file
+    cat ../provisioning/build/cfn/${filename}.yaml | cfn-guard validate -r ${file}
 done
