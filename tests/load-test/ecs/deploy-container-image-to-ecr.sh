878c61493f7be4faaac5a1ffd2ef5b65e8abef94
TomoyukiSugiyama <s_tomoyuki07@yahoo.co.jp>
2022-05-08 18:50:06 +0900

    add deploy scripts

---
+++ tests/load-test/ecs/deploy-container-image-to-ecr.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+docker context use default
+
+aws ecr get-login-password --region ${AWS_REGION}| docker login --username AWS --password-stdin ${ECR_URI}
+
+for SERVICE in locust;
+do
+  docker image build -t ${ECR_URI}:${SERVICE} ${SERVICE}/
+  docker image push ${ECR_URI}:${SERVICE}
+done
